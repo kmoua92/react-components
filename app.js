ca87b0f098b514310e8e5741bae004eefafedc3c
kmoua92 <kermoua92@gmail.com>
2016-09-30 16:28:49 -0700

    Complete all prompts

--- app.js
+++ app.js
@@ -1,1 +1,67 @@
 // TODO
+// var Apples = () => (
+//   <li>Apples</li>
+// );
+
+// var Bananas = () => (
+//   <li>Bananas</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false,
+      hover: false
+    };
+  }
+
+  itemClicked (event) {
+    this.setState({
+      done: !this.state.done
+    })
+  }
+
+  mouseEntered (event) {
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
+
+  mouseLeft (event) {
+    this.setState({
+      hover: !this.state.hover
+    })
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal'
+    };
+
+    return (
+        <li style={style} onClick={this.itemClicked.bind(this)} onMouseEnter={this.mouseEntered.bind(this)} onMouseLeave={this.mouseLeft.bind(this)}>{this.props.item}</li>
+    );
+  }
+};
+
+let GroceryList = (props) => (
+  <ul>
+    {props.groceries.map(item =>
+      <GroceryListItem item={item}/>
+    )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryList groceries={['apples','bananas','oranges', 'juice', 'milk', 'bread']} />, document.getElementById('app'));
+
+
+
+
+
+
+
+
+
